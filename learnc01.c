38de3066baca360c74c2ca0b11f210e015f5ae7d
Karlvin Lapuz <kllapuz3@student.apc.edu.ph>
2019-11-04 02:49:44 +0000

    initial commit for learnc01.c

---
+++ learnc01.c
@@ -0,0 +1,11 @@
+
+int main()
+{
+    int a, b, c;
+    printf("Enter X coordinate:");
+    scanf("%d", &a);
+    printf("Enter Y coordinate:");
+    scanf("%d", &b);
+    printf("Your Coodinates: %d, %d \n", a, b);
+    return 0;
+}
